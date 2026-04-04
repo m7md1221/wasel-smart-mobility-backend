cfcefcb4d9b7d74046274d1e8f94584754fbdbf0
marahsirhed <s12216999@stu.najah.edu>
2026-04-04 18:59:54 +0300

    alerts model fix

--- src/models/alertModel.js
+++ src/models/alertModel.js
@@ -2,12 +2,34 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const Alert = sequelize.define("Alert", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  incident_id: DataTypes.INTEGER,
-  created_at: DataTypes.DATE
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  incident_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "incidents", 
+      key: "id",
+    },
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  message: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
 }, {
   tableName: "alerts",
-  timestamps: false
+  timestamps: false, 
 });
 
 module.exports = Alert;
