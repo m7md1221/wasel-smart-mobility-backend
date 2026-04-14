b43e9c6409dfc5f0720774fe35fb7d31a7603545
marahsirhed <s12216999@stu.najah.edu>
2026-04-14 12:36:46 +0300

    user activation and deactivation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -257,4 +257,66 @@ async function deleteUser(req,res){
     });
   }
 }
-module.exports = { addUser, showUserInfo, showAllUsers, updateUser, deleteUser,signup,login };
+async function deactivateUser(req,res){
+   try{
+    const id = req.params.id;
+    if (isNaN(id)) {
+      return res.status(400).json({
+        message: "Invalid user ID"
+      });
+    }
+    const user = await User.findByPk(id);
+    if(!user){
+      return res.status(404).json({
+        message : "User not found"
+      });
+    }
+    if(user.is_active === false){
+      return res.status(400).json({
+        message : "User is already deactivated"
+      });
+    }
+    await user.update({ is_active: false, updated_at: new Date() });
+    res.status(200).json({
+      message : "User deactivated successfully"
+    });
+  } catch(error){
+    console.error("[User] Error deactivating user:", error);
+    return res.status(500).json({
+      message: "Error deactivating user",
+      error: error.message
+    });
+  }
+}
+  async function activateUser(req,res){
+   try{
+    const id = req.params.id;
+    if (isNaN(id)) {
+      return res.status(400).json({
+        message: "Invalid user ID"
+      });
+    } 
+    const user = await User.findByPk(id);
+    if(!user){
+      return res.status(404).json({
+        message : "User not found"
+      });
+    }
+    if(user.is_active === true){
+      return res.status(400).json({
+        message : "User is already active"
+      });
+    }
+    await user.update({ is_active: true, updated_at: new Date() });
+    res.status(200).json({
+      message : "User activated successfully"
+    });
+  } catch(error){
+    console.error("[User] Error activating user:", error);
+    return res.status(500).json({
+      message: "Error activating user",
+      error: error.message
+    });
+  }
+}
+module.exports = { addUser, showUserInfo, showAllUsers, updateUser, deleteUser, deactivateUser, activateUser, signup, login };
