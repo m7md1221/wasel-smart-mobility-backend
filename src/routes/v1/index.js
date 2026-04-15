05136265a99f474171091f22b0529d5982421b56
marahsirhed <s12216999@stu.najah.edu>
2026-04-15 22:46:39 +0300

    alerts service and route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const userRouter = require('../users');
 const routingRouter = require("../routes");
 const subscribeRouter = require("../subscribe");
+const alertsRouter = require("../alerts");
 const router = express.Router();
+
 const app=express();
 app.use(express.json());
+//routes
 router.use("/users", userRouter);
-router.use("/routes", routingRouter); // keep old path for compatibility
-router.use("/alertSubscriptions", subscribeRouter); // make /api/v1/alertSubscriptions routes available directly
+router.use("/routes", routingRouter); 
+router.use("/alertSubscriptions", subscribeRouter); 
+router.use("/alerts", alertsRouter);
 
 // healthcheck
 router.get("/health", (req, res) => {
