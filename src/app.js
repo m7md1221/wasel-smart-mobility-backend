8fa437927d71f58f9b8860ad9a3f2872acde9272
marahsirhed <s12216999@stu.najah.edu>
2026-04-22 13:34:23 +0300

    testing routes documentation

--- src/app.js
+++ src/app.js
@@ -9,15 +9,17 @@ app.use(express.json());
 app.use(bodyparser.json());
 //testing for webhook 
 app.get("/test", (req, res) => {
+  // #swagger.tags = ['Testing']
+  // #swagger.summary = 'Test endpoint to verify the main app is working'
   res.json({ message: "main app works" });
 });
-
-
 // Routes
 app.use("/api/v1", v1Routes);
 
 //swagger documentation as json file
 app.get('/swagger-output.json', (req, res) => {
+  // #swagger.tags = ['API Documentation']
+  // #swagger.summary = 'The generated Swagger documentation JSON file'
   res.json(require('../swagger-output.json'));
 });
 //API DOCUMENTATION
